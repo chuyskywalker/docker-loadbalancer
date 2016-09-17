3b6fa41a9e47fe79410e15a13e68c1f033b8d7af
Jeff Minard <jeff@jrm.cc>
2016-09-17 15:45:14 -0700

    Use local consul now

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -ex
 
-consul lock \
- -http-addr=192.168.1.51:8500 \
- -http-addr=192.168.1.52:8500 \
- -http-addr=192.168.1.53:8500 \
- -n=1 -verbose \
- webserver /claim.sh
+consul lock -n=1 -verbose webserver /claim.sh
