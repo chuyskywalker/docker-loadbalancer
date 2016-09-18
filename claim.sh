40a672c1ae514b41e7ff18ea6edf00628c62a208
Jeff Minard <jeff@jrm.cc>
2016-09-17 19:34:45 -0700

    Properly apply, commit, and restart the firewall to pickup new lb location

--- claim.sh
+++ claim.sh
@@ -4,7 +4,8 @@ MYIP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 
 echo `date` "Claiming web ownership as $MYIP..."
 
-/usr/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no admin@192.168.1.1 'nvram set vts_rulelist="<clusterfollow>80>'${MYIP}'>80>TCP"'
+/usr/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no admin@192.168.1.1 \
+ 'nvram set vts_rulelist="<clusterfollow>80>'${MYIP}'>80>TCP"; nvram commit; service restart_firewall'
 
 echo `date` "...Claimed! Sleeping forever now kthnxbye!"
 
